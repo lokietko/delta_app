d846d0c2e978cbf46829b056588888d9c199c254
anna <anna.lokietko@gmail.com>
2019-02-12 21:15:14 +0100

    oferta cd

--- src/Ofert.js
+++ src/Ofert.js
@@ -30,7 +30,7 @@ handleIconMouseOver=(e)=>{
     render() { 
         return ( <>    
     <h1 className="ofertTitle">Oferta</h1>
-    <h2>BADANIA PSYCHOLOGICZNE Z ZAKRESU MEDYCYNY PRACY DLA:</h2>
+    <h2 className='titleH2'>BADANIA PSYCHOLOGICZNE Z ZAKRESU MEDYCYNY PRACY DLA:</h2>
     <div className="forWho">
         <div className='flexicon'> <i className = "fas fa-car" onMouseOver={this.handleIconMouseOver} onMouseLeave={this.handleIconMouseLeave}> </i><p className="indoIcons">kierowców</p>
                {/* <ul class='driver'>
@@ -49,6 +49,22 @@ handleIconMouseOver=(e)=>{
         <div className='flexicon'><i className = "fas fa-dolly-flatbed" onMouseOver={this.handleIconMouseOver} onMouseLeave={this.handleIconMouseLeave}> </i><p className="indoIcons">operatorów maszyn,<br></br> kierowców wózków widłowych, operatorów suwnic,<br></br> operatorów koparek, itp.</p></div>
         <div className='flexicon'><i className="fas fa-file-medical" onMouseOver={this.handleIconMouseOver} onMouseLeave={this.handleIconMouseLeave}></i><p className="indoIcons">badania okresowe i kontrolne</p></div>
     </div>
+<h2>BADANIA LEKARSKIE Z ZAKRESU MEDYCYNY PRACY:</h2>
+<p>badania lekarskie z zakresu medycyny pracy na wymienione wyżej stanowiska.</p>
+<h2>KONSULTACJE I PORADY (TAKŻE DROGĄ TELEFONICZNĄ I INTERNETOWĄ)</h2>
+<p>– konsultacje dla uczniów, studentów i absolwentów, którzy chcą odnaleźć właściwą sobie drogę w dorosłym życiu,
+
+– konsultacje motywujące, skoncentrowane na poszukiwaniu i wzmacnianiu swoich mocnych stron (coaching),
+
+– poradnictwo w przypadkach nagłych,
+
+– konsultacje w sytuacjach kryzysowych, stanach depresyjnych, problemach z odnalezieniem swojego miejsca w świecie,
+
+– konsultacje skoncentrowane na odnajdywaniu mocnych stron w sytuacjach zawodowych (poszukiwanie pracy, przygotowanie do rozmów kwalifikacyjnych).</p>
+
+
+
+
     </> );
     }
 }
