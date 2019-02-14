11b7372e3746873b576a0d660cb53cdc5e3b66e5
anna <anna.lokietko@gmail.com>
2019-02-14 20:32:02 +0100

    scroll

--- src/kontakt.js
+++ src/kontakt.js
@@ -6,7 +6,16 @@ import "./kontakt.css";
          this.state = {  }
      } 
      
+handleScroll=()=>{
+const tr=document.getElementsByClassName("tringlesSides");
+const win=window;
+console.log(tr,win);
+win.addEventListener("scroll",function(){
+  //  let top=win.srollTop();
+    console.log(win.scrollBy());
+})
 
+}
 
 
 
@@ -17,7 +26,7 @@ import "./kontakt.css";
          
          <div className="tringlesSides" data-speed="2"></div>
          
-         
+         {this.handleScroll()}
          
          
          
